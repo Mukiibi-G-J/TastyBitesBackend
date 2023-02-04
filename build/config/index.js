1178f682632abae5dbe61486f97ded0530eb34a7
unknown <m-g-j@m-g-jose.lubagahospital.org>
2023-02-04 20:24:12 +0300

    Building project

---
+++ build/config/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.APP_SECRET = exports.PORT = exports.MONGO_URI = void 0;
+// mongodb://localhost:27017/online_food_delivery
+// export const MONGO_URI = 'mongodb://localhost:27017/online_food_delivery';
+exports.MONGO_URI = 'mongodb+srv://admin:admin@cluster0.wmapd.mongodb.net/test';
+// 'mongodb://127.0.0.1:27017/online_food_delivery'
+exports.PORT = process.env.PORT || 8000;
+exports.APP_SECRET = 'Our_APP_Secret';
+//# sourceMappingURL=index.js.map
---
+++ build/routes/CustomerRoute.js
@@ -0,0 +1,28 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CustomerRoute = void 0;
+var express_1 = __importDefault(require("express"));
+var controllers_1 = require("../controllers");
+var middlewares_1 = require("../middlewares");
+// import { AddToCart, CreateOrder, CreatePayment, CustomerLogin, CustomerSignUp, CustomerVerify, DeleteCart, EditCustomerProfile, GetCart, GetCustomerProfile, GetOrderById, GetOrders, RequestOtp, VerifyOffer } from '../controllers';
+// import { Authenticate } from '../middleware';
+// import { Offer } from '../models/Offer';
+var router = express_1.default.Router();
+exports.CustomerRoute = router;
+/* ------------------- Suignup / Create Customer --------------------- */
+router.post('/signup', controllers_1.CustomerSignUp);
+/* ------------------- Login --------------------- */
+router.post('/login', controllers_1.CustomerLogin);
+// /* ------------------- Authentication --------------------- */
+router.use(middlewares_1.Authenticate);
+// /* ------------------- Verify Customer Account --------------------- */
+router.patch('/verify', controllers_1.CustomerVerify);
+// /* ------------------- OTP / request OTP --------------------- */
+router.get('/otp', controllers_1.RequestOtp);
+// /* ------------------- Profile --------------------- */
+router.get('/profile', controllers_1.GetCustomerProfile);
+router.patch('/profile', controllers_1.EditCustomerProfile);
+//# sourceMappingURL=CustomerRoute.js.map
