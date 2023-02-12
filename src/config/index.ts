33df8e2d48e5713170e3c7fb2483578d11eb3d10
unknown <m-g-j@m-g-jose.lubagahospital.org>
2023-02-12 10:34:52 +0300

    fix twillo notification

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 // mongodb://localhost:27017/online_food_delivery
-export const MONGO_URI = 'mongodb://127.0.0.1:27017/online_food_delivery';
-// export const MONGO_URI = 'mongodb+srv://admin:admin@cluster0.wmapd.mongodb.net/test'
+// export const MONGO_URI = 'mongodb://127.0.0.1:27017/online_food_delivery';
+export const MONGO_URI = 'mongodb+srv://admin:admin@cluster0.wmapd.mongodb.net/test'
 // 'mongodb://127.0.0.1:27017/online_food_delivery'
  
 export const PORT = process.env.PORT || 8000;
