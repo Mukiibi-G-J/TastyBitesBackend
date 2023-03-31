dd1f0fd0daac534cea59043137057cb6fbc8eea4
unknown <m-g-j@m-g-jose.lubagahospital.org>
2023-03-31 21:11:48 +0300

    commented Twillo OTP featuer

--- src/utils/NotificationUtility.ts
+++ src/utils/NotificationUtility.ts
@@ -15,19 +15,19 @@ export const GenerateOtp = () => {
 export const onRequestOTP = async (otp: number, toPhoneNumber: string) => {
 //   //! 'Your Account SID from TWILIO DASHBOARD'
 //   // const accountSid = 'ACd9abf1bc98db292070ea30bc01e8a495';
-//   const accountSid = "AC3c0255c2bd83829955dd5bfe03e16806";
-//   // !'YOUR AUTH TOKEN AS I SAID ON VIDEO';
-//   const authToken = "4df20dd25e45cb3aa090b5a609920631";
-//   // const authToken = '0b5190df790bc9ce2a7fd4a2587badeb';
-//   const client = require("twilio")(accountSid, authToken);
-//   const response = await client.messages.create({
-//     body: `Your OTP is ${otp}`,
-//     // from: 'Your TWILIO PHONE NUMBER YOU CAN GET IT FROM YOUR DASHBOARD',
-//     // from: '+15075449854',
-//     from: "+14344437873",
+  const accountSid = "AC3c0255c2bd83829955dd5bfe03e16806";
+  // !'YOUR AUTH TOKEN AS I SAID ON VIDEO';
+  const authToken = "4df20dd25e45cb3aa090b5a609920631";
+  // const authToken = '0b5190df790bc9ce2a7fd4a2587badeb';
+  const client = require("twilio")(accountSid, authToken);
+  // const response = await client.messages.create({
+  //   body: `Your OTP is ${otp}`,
+  //   // from: 'Your TWILIO PHONE NUMBER YOU CAN GET IT FROM YOUR DASHBOARD',
+  //   // from: '+15075449854',
+  //   from: "+14344437873",
 
-//     to: `+256${toPhoneNumber}`, // recipient phone number // Add country before the number
-//   });
+  //   to: `+256${toPhoneNumber}`, // recipient phone number // Add country before the number
+  // });
   const response =  {
     otp: otp,
 
